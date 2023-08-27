475f780e38b7882e1d54647c7d63466a1a99259f
lucas.singh <lucas.singh@bizitglobal.com>
2023-08-27 20:40:06 -0300

    cambios

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -137,7 +137,7 @@ export default function Projects() {
                                 <span>
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                                 </span>
-                                <img src={img} alt="arq" style={{marginBottom:12}}/>
+                                <img src={img} alt="arq" style={{marginTop:12}}/>
                             </div>
                             <div className='footer'>
                                 <a className='link' href="google.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
